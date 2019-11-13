9ecc8a73f9f26e28890a4749bb5aeb082f9c7932
Nishad <md.nesaruddinsikder@gmail.com>
2019-11-13 23:15:15 +0600

    Add Categoris Service and with in Save and Get Category

---
+++ bAnglaShopar.Com.Web/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using bAnglaShoper.Com.Services;
+using bAnglaShoper.ComEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bAnglaShopar.Com.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoriesService categoriesService = new CategoriesService();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var categoryList = categoriesService.GetCategory();
+            return View(categoryList);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            categoriesService.SaveCategory(category);
+            return View();
+        }
+    }
+}
---
+++ bAnglaShoper.Com.Services/CategoriesService.cs
@@ -0,0 +1,33 @@
+﻿using bAnglaShoper.Com.DataBase;
+using bAnglaShoper.ComEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bAnglaShoper.Com.Services
+{
+    public class CategoriesService
+    {
+        public List<Category> GetCategory()
+        {
+            using (var context = new ProjectDBContext())
+            {
+                return context.Categories.ToList();
+                
+            }
+           
+        }
+        public void SaveCategory(Category category)
+        {
+            using (var context = new ProjectDBContext())
+            {
+                //Add category
+                context.Categories.Add(category);
+                //Save Changes
+                context.SaveChanges();
+            }
+        }
+    }
+}
