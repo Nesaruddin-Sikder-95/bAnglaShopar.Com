f672518e3f5ef84950ade1c1e58b9a40d2309b0b
Nishad <md.nesaruddinsikder@gmail.com>
2019-11-14 23:11:33 +0600

    Add Product Controller and Product Service with theres different method

---
+++ bAnglaShopar.Com.Web/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using bAnglaShoper.Com.Services;
+using bAnglaShoper.ComEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bAnglaShopar.Com.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // Show product in table
+        public ActionResult ProductTable(string search)
+        {
+            var products = productsService.GetProducts();
+            return PartialView(products);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            // we need only value not all page thats why use Partial keyword
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productsService.SaveProduct(product);
+            return RedirectToAction("ProductTable");
+        }
+    }
+}
---
+++ bAnglaShoper.Com.Services/ProductsService.cs
@@ -0,0 +1,69 @@
+﻿using bAnglaShoper.Com.DataBase;
+using bAnglaShoper.ComEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bAnglaShoper.Com.Services
+{
+    public class ProductsService
+    {
+        //Single Product
+        public Product GetProduct(int ID)
+        {
+            using (var context = new ProjectDBContext())
+            {
+                return context.Products.Find(ID);
+
+            }
+
+        }
+        //All Products
+        public List<Product> GetProducts()
+        {
+            using (var context = new ProjectDBContext())
+            {
+                return context.Products.ToList();
+                
+            }
+           
+        }
+        public void SaveProduct(Product product)
+        {
+            using (var context = new ProjectDBContext())
+            {
+                //Add Product
+                context.Products.Add(product);
+                //Save Changes
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new ProjectDBContext())
+            {
+                //Update Product
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified; 
+                //Save Changes
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteProduct(int ID)
+        {
+            using (var context = new ProjectDBContext())
+            {
+                //Update product
+                //context.Entry(product).State = System.Data.Entity.EntityState.Deleted;
+                var product = context.Products.Find(ID);
+                context.Products.Remove(product);
+
+                //Save Changes
+                context.SaveChanges();
+            }
+        }
+    }
+}
