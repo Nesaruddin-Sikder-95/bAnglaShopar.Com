955ac265c846d8ac3215db4ac9d0973791ec1749
Nishad <md.nesaruddinsikder@gmail.com>
2019-11-21 23:55:28 +0600

    Adding a Shared Controller for Image Upload

---
+++ bAnglaShopar.Com.Web/Controllers/SharedController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bAnglaShopar.Com.Web.Controllers
+{
+    public class SharedController : Controller
+    {
+        public JsonResult UploadImage()
+        {
+            JsonResult result = new JsonResult();
+            try
+            {
+                var file = Request.Files[0];
+                var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/Content/images/"), fileName);
+                file.SaveAs(path);
+                result.Data = new { Success = true, ImageURL = path };
+                
+
+            }
+            catch (Exception ex)
+            {
+                result.Data = new { Success = false, Message = ex.Message };
+            }
+            return result;
+        }
+
+    }
+}
